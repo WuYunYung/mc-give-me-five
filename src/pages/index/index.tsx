6b126586eb9cb5532c7ded0269d0701d90268b61
WilsonWu <wilson.wu.yy@gmail.com>
2024-11-01 13:26:47 +0800

    chore: 引入biome

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,15 +1,15 @@
-import { View, Text } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
-import './index.css'
+import { View, Text } from "@tarojs/components";
+import { useLoad } from "@tarojs/taro";
+import "./index.css";
 
-export default function Index () {
-  useLoad(() => {
-    console.log('Page loaded.')
-  })
+export default function Index() {
+	useLoad(() => {
+		console.log("Page loaded.");
+	});
 
-  return (
-    <View className='index'>
-      <Text className='text-red-500'>Hello world!</Text>
-    </View>
-  )
+	return (
+		<View className="index">
+			<Text className="text-red-500">Hello world!</Text>
+		</View>
+	);
 }
