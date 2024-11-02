f9e1d5c85a3ba62dac1850617a4bd6693a49bdcb
wuyunho <1458565220@qq.com>
2024-11-02 20:18:56 +0800

    feat(tabbar): 添加tabbar
    (cherry picked from commit fd02045bd4aa5087a04df07598e0c10c6871bdd6)

---
+++ src/pages/user/index.tsx
@@ -0,0 +1,12 @@
+import { View } from '@tarojs/components'
+import React from 'react'
+
+definePageConfig({
+  navigationBarTitleText: "我的",
+})
+
+export default function User() {
+	return (
+		<View>Home</View>
+	)
+}
