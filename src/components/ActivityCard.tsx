9463be0e8e2d318547983c6cccb70a05adfb8990
WilsonWu <wilson.wu.yy@gmail.com>
2024-11-06 15:34:16 +0800

    feat: 卡片cell替换

--- src/components/ActivityCard.tsx
+++ src/components/ActivityCard.tsx
@@ -7,6 +7,7 @@ import banner1 from "../static/banner/banner1.svg";
 import banner2 from "../static/banner/banner2.svg";
 import banner3 from "../static/banner/banner3.svg";
 import banner4 from "../static/banner/banner4.svg";
+import { Cell } from "@taroify/core";
 
 type CardMode = "activity" | "history";
 
@@ -26,49 +27,28 @@ export default function ActivityCard({
 }: { activityDetail: ActivityRead; mode: CardMode }) {
 	return (
 		<View
-			className="box-border flex flex-col w-full shadow-xl rounded-lg mb-4"
+			className="box-border flex flex-col w-full shadow-xl rounded-lg mb-4 overflow-hidden"
 			onClick={() => {
 				routePush(`/${mode}/pages/${mode}-detail`, {
 					id: activityDetail.id,
 				});
 			}}
 		>
-			<View className="flex w-full h-32 rounded-t-lg box-border border-solid border-gray-300">
+			<View className="flex w-full h-32 box-border border-solid border-gray-300">
 				<Image className="w-32 h-32" src={bannerMap[activityDetail.type]} />
 				<Text className="m-auto font-bold text-xl">{activityDetail.name}</Text>
 			</View>
-			<View className="flex flex-col px-2">
-				<View className="flex w-full h-12 border-b-2 border-x-0 border-t-0 border-solid  border-slate-100">
-					<Text className="my-auto">负责人</Text>
-					<Text className="my-auto ml-auto text-slate-400">
-						{activityDetail.creator.name}
-					</Text>
-				</View>
-				<View className="flex w-full h-12 border-b-2 border-x-0 border-t-0 border-solid  border-slate-100">
-					<Text className="my-auto">活动名额</Text>
-					<Text className="my-auto ml-auto text-slate-400">
-						{activityDetail.get_attenders_count} / {activityDetail.capacity}
-					</Text>
-				</View>
-				<View className="flex w-full h-12 border-b-2 border-x-0 border-t-0 border-solid  border-slate-100">
-					<Text className="my-auto">活动地点</Text>
-					<Text className="my-auto ml-auto text-slate-400">
-						{activityDetail.location}
-					</Text>
-				</View>
-				<View className="flex w-full h-12 border-b-2 border-x-0 border-t-0 border-solid  border-slate-100">
-					<Text className="my-auto">开始时间</Text>
-					<Text className="my-auto ml-auto text-slate-400">
-						{dayjs(activityDetail.start_time).format("YYYY-MM-DD HH:mm")}
-					</Text>
-				</View>
-				<View className="flex w-full h-12 border-b-2 border-x-0 border-t-0 border-solid  border-slate-100">
-					<Text className="my-auto">结束时间</Text>
-					<Text className="my-auto ml-auto text-slate-400">
-						{dayjs(activityDetail.end_time).format("YYYY-MM-DD HH:mm")}
-					</Text>
-				</View>
-			</View>
+			<Cell.Group bordered={false}>
+				<Cell title="负责人">{activityDetail.creator.name}</Cell>
+				<Cell title="活动名额">{`${activityDetail.get_attenders_count} / ${activityDetail.capacity}`}</Cell>
+				<Cell title="活动地点">{activityDetail.location}</Cell>
+				<Cell title="开始时间">
+					{dayjs(activityDetail.start_time).format("YYYY-MM-DD HH:mm")}
+				</Cell>
+				<Cell title="结束时间">
+					{dayjs(activityDetail.end_time).format("YYYY-MM-DD HH:mm")}
+				</Cell>
+			</Cell.Group>
 		</View>
 	);
 }
