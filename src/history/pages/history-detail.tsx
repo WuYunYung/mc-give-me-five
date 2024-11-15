9433a1b31cfec073272b3d89149595d0a31b1921
WilsonWu <wilson.wu.yy@gmail.com>
2024-11-15 10:09:41 +0800

    style(history-detail): 代码&样式优化

--- src/history/pages/history-detail.tsx
+++ src/history/pages/history-detail.tsx
@@ -1,14 +1,47 @@
 import { activityAttend, activityQuit, activityRead } from "@/api";
 import { View } from "@tarojs/components";
 import ActivityDetailCard from "../../components/ActivityDetailCard";
-import { Button, SafeArea } from "@taroify/core";
+import { Button, Divider, SafeArea } from "@taroify/core";
 import dayjs from "dayjs";
-import { useRouter, showModal, setNavigationBarTitle } from "@tarojs/taro";
+import {
+	useRouter,
+	showModal,
+	setNavigationBarTitle,
+	showToast,
+} from "@tarojs/taro";
 import { useRequest } from "ahooks";
 import { routePush } from "@/shared/route";
 import useStore from "@/shared/store";
 import useBackShow from "@/hooks/useBackShow";
 import { showToastAsync } from "@/shared/utils";
+import { FC, PropsWithChildren, useMemo } from "react";
+import { Checked, Warning } from "@taroify/icons";
+
+const ActivityStatus: FC<
+	PropsWithChildren<{
+		type: "success" | "warning";
+	}>
+> = (props) => {
+	const { type, children } = props;
+
+	const iconSize = 16;
+
+	const icon =
+		type === "success" ? (
+			<Checked className="text-green-600" size={iconSize} />
+		) : (
+			<Warning className="text-gray-600" size={iconSize} />
+		);
+
+	return (
+		<Divider>
+			<View className="flex gap-2 items-center">
+				{icon}
+				{children}
+			</View>
+		</Divider>
+	);
+};
 
 export default function () {
 	const { params } = useRouter();
@@ -18,8 +51,8 @@ export default function () {
 	const { data: activity, refresh } = useRequest(activityRead, {
 		defaultParams: [Number(id)],
 		ready: !!id,
-		onSuccess() {
-			setNavigationBarTitle({ title: `${activity.name}` });
+		onSuccess(res) {
+			setNavigationBarTitle({ title: `${res.name}` });
 		},
 	});
 
@@ -29,7 +62,7 @@ export default function () {
 		manual: true,
 		defaultParams: [Number(id)],
 		onSuccess() {
-			showToastAsync({
+			showToast({
 				title: "签到成功",
 				icon: "success",
 			});
@@ -79,7 +112,6 @@ export default function () {
 
 	//老师生成签到码
 	const handleFindCode = () => {
-		// findCode(Number(id));
 		routePush("/history/pages/detail/detail-qrcode", {
 			id: id,
 		});
@@ -103,74 +135,80 @@ export default function () {
 
 	const { isAdmin } = user || {};
 
-	const renderButtons = (
+	const activityStatus = useMemo(() => {
+		const { start_time, end_time } = activity || {};
+
+		const now = new Date();
+
+		const compareUnit = "minute" as const;
+
+		const started = start_time && dayjs(start_time).isBefore(now, compareUnit);
+		const finish = end_time && dayjs(end_time).isBefore(now, compareUnit);
+
+		return {
+			started,
+			inProgress: started && !finish,
+			finish,
+		};
+	}, [activity]);
+
+	const studentAttendEntries = (
 		<>
-			{/* 学生-未报名活动 */}
-			{!isAdmin && !activity?.is_attend && (
-				<View className="p-4">
-					<Button
-						color="primary"
-						block
-						onClick={() => handleAttendActivity(Number(id))}
-					>
-						报名
+			{/* 学生-活动未开始 */}
+			{!activityStatus.started && (
+				<>
+					<ActivityStatus type="success">已报名</ActivityStatus>
+
+					<Button block color="default" onClick={handleQuit}>
+						取消报名
 					</Button>
-				</View>
+				</>
 			)}
 
-			{/* 学生-已报名活动 */}
-
-			{/* 学生-活动未开始 */}
-			{!isAdmin &&
-				activity &&
-				activity?.is_attend &&
-				dayjs(activity.start_time).valueOf() > dayjs().valueOf() && (
-					<View className="flex flex-col gap-4 p-4">
-						<Button block color="success">
-							已报名
-						</Button>
-						<Button block color="default" onClick={handleQuit}>
-							取消报名
-						</Button>
-					</View>
-				)}
-
 			{/* 学生-活动已开始未结束 */}
-			{!isAdmin &&
-				activity &&
-				activity?.is_attend &&
-				dayjs(activity.start_time).valueOf() < dayjs().valueOf() &&
-				dayjs(activity.end_time).valueOf() > dayjs().valueOf() && (
-					<View className="flex flex-col gap-4 p-4">
-						<Button block color="success">
-							{activity.is_signed ? "已签到" : "已报名"}
-						</Button>
-					</View>
-				)}
+			{activityStatus.inProgress && (
+				<ActivityStatus type="success">
+					{activity?.is_signed ? "已签到" : "已报名"}
+				</ActivityStatus>
+			)}
 
 			{/* 学生-活动已结束 */}
-			{!isAdmin &&
-				activity &&
-				activity?.is_attend &&
-				dayjs(activity.end_time).valueOf() < dayjs().valueOf() && (
-					<View className="flex flex-col gap-4 p-4">
-						{activity.is_signed && (
-							<Button block color="primary">
-								已签到
-							</Button>
-						)}
+			{activityStatus.finish && (
+				<>
+					{activity?.is_signed ? (
+						<ActivityStatus type="success">已签到</ActivityStatus>
+					) : (
+						<ActivityStatus type="warning">已结束（未签到）</ActivityStatus>
+					)}
+				</>
+			)}
+		</>
+	);
 
-						{!activity.is_signed && (
-							<Button block color="warning">
-								已结束（未签到）
-							</Button>
-						)}
-					</View>
-				)}
+	const studentEntries = (
+		<>
+			{/* 学生-未报名活动 */}
+			{!activity?.is_attend ? (
+				<Button
+					color="primary"
+					block
+					onClick={() => handleAttendActivity(Number(id))}
+				>
+					报名
+				</Button>
+			) : (
+				studentAttendEntries
+			)}
+		</>
+	);
+
+	const renderButtons = (
+		<View className="flex flex-col p-4 gap-4">
+			{!isAdmin && studentEntries}
 
 			{/* 老师-管理活动 */}
 			{isAdmin && activity && (
-				<View className="flex flex-col p-4 gap-4">
+				<>
 					<Button block color="primary" onClick={handleFindCode}>
 						生成签到码
 					</Button>
@@ -190,9 +228,9 @@ export default function () {
 					>
 						查看详情
 					</Button>
-				</View>
+				</>
 			)}
-		</>
+		</View>
 	);
 
 	return (
